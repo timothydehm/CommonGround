e6c2b25fc311e3bae6fe08d3b4955d313053d7c2
timothydehm <tim.dehm@gmail.com>
2025-06-14 22:58:02 -0400

    Add parcel selection functionality with color toggling

--- public/script.js
+++ public/script.js
@@ -7,13 +7,37 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 fetch('parcels.geojson')
   .then(response => response.json())
   .then(data => {
-    const geojsonLayer = L.geoJSON(data, {
-      style: {
+    function getDefaultStyle() {
+      return {
         color: '#555',
         weight: 1,
         fillColor: '#ccc',
         fillOpacity: 0.7
-      }
+      };
+    }
+
+    function getSelectedStyle() {
+      return {
+        color: '#0057e7',
+        weight: 2,
+        fillColor: '#0099ff',
+        fillOpacity: 0.9
+      };
+    }
+
+    function onEachFeature(feature, layer) {
+      layer._selected = false;
+
+      layer.on('click', function () {
+        layer._selected = !layer._selected;
+
+        layer.setStyle(layer._selected ? getSelectedStyle() : getDefaultStyle());
+      });
+    }
+
+    const geojsonLayer = L.geoJSON(data, {
+      style: getDefaultStyle,
+      onEachFeature: onEachFeature
     }).addTo(map);
 
     map.fitBounds(geojsonLayer.getBounds());
